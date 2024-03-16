1fd61307997ed24b2ffd793cd512c760e58f04e7
KacperKozicki <kacper.kozicki@microsoft.wsei.edu.pl>
2024-03-16 12:17:29 +0100

    Ścieżki, tykacz, nagrania dla scieżek

--- lab3/script.js
+++ lab3/script.js
@@ -8,27 +8,83 @@ const sounds={
     's':document.querySelector('#sound2'),
     'd':document.querySelector('#sound3'),
     'f':document.querySelector('#sound4'),
-}
+    'g':document.querySelector('#sound5'),
 
-const times=[]
+    'z':document.querySelector('#sound6'),
+    'x':document.querySelector('#sound7'),
+    'c':document.querySelector('#sound8'),
+    'v':document.querySelector('#sound9'),
+}
+//tablica tablic obiektów, dla każdej ścieżki własna tablica a wniej obiekt 
+const times = [[], [], [], []];
+
+//tablica checkboxów
+const checkbox = [
+    document.querySelector('#track1'),
+    document.querySelector('#track2'),
+    document.querySelector('#track3'),
+    document.querySelector('#track4')
+];
 
 
 const start = document.querySelector('#start');
 const end = document.querySelector('#stop');
 const time = document.querySelector('#time');
-const track1 = document.querySelector('#Track1');
+const tykacz = document.querySelector('#tykacz');
+
+tykacz.addEventListener('change',()=>{
+    if(tykacz.checked){
+        console.log("Tykanie");
+        tykaczInterval = setInterval(() => {
+            sounds['a'].currentTime = 0;
+            sounds['a'].play();
+        }, 500);
+    } else {
+        if (tykaczInterval) {
+            clearInterval(tykaczInterval); 
+        }
+    }
+    })
+        
+            
+
+
+const play = document.querySelector('#play');
 
 let startTime
 let endTime
 let isRecording=false
-// const playTrack = document.querySelector('')
+const playTrack = document.querySelector('#time')
 
+//odpalamy scieżke po checkboxie
+play.addEventListener('click', () => {
+    checkbox.forEach((checkbox, index) => {
+        if (checkbox.checked) {
+            playTracks(times[index]);
+        }
+    });
+});
 
+function playTracks(timesArray) {
+    timesArray.forEach(element => {
+        const soundToPlay = sounds[element.key];
+        if (soundToPlay) {
+            setTimeout(() => {
+                soundToPlay.currentTime = 0;
+                soundToPlay.play();
+            }, element.time);
+        }
+    });
+}
+
+//nagrywaj
 start.addEventListener('click',()=>{
     startTime = Date.now();
     isRecording=true
+    currentTrackIndex = checkbox.findIndex(checkbox => checkbox.checked); //znajdz zaznaczone checkboxy
 })
 
+//koniec nagrania - czas
 end.addEventListener('click',()=>{
     endTime= Date.now();   
     isRecording=false
@@ -36,26 +92,26 @@ end.addEventListener('click',()=>{
     time.textContent=timeResult
 })
 
-    addEventListener('keypress',(ev)=> {
-        if(isRecording){
-            const key =ev.key
-            //times[ev.key]=Date.now()
-        
-            times.push({
-                key: ev.key,
-                time: Date.now() - startTime
-            })
-            const sound = sounds[key]
-            sound.currentTime = 0
-            sound.play()
-            
+addEventListener('keypress',(ev)=> {
+    if(isRecording){
+        const key =ev.key
+        //times[ev.key]=Date.now()
+
+        const sound = sounds[key];
+        if (sound) {
+            times[currentTrackIndex].push({
+                key: key,
+                time: Date.now() - startTime,
+            });
+            sound.currentTime = 0;
+            sound.play();
         }
-    })    
+    }
+})    
 
 console.log(times)
 
 
-addEventListener
 
 
 
@@ -88,3 +144,4 @@ addEventListener
     // console.log(key)
     // s1.currentTime=0
     // s1.play()
+    
